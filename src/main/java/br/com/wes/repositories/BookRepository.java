80f68b815afd213bc3977e660039d301a7b6bddb
hiagobalbino <hiagobalbinoo@gmail.com>
2023-12-28 11:55:59 -0300

    :construction: feat: implement Book repository

---
+++ src/main/java/br/com/wes/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package br.com.wes.repositories;
+
+import br.com.wes.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {}
