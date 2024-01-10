cf1bb1cd5d0570aca185c3bc8309f21c2d14afde
hiagobalbino <hiagobalbinoo@gmail.com>
2024-01-09 22:10:39 -0300

    test: implement generation of token on startup for testing purpose

--- src/main/java/br/com/wes/BoperegApplication.java
+++ src/main/java/br/com/wes/BoperegApplication.java
@@ -8,6 +8,20 @@ public class BoperegApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoperegApplication.class, args);
+
+//		generatePassword();
 	}
 
+//	private static void generatePassword() {
+//		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+//
+//		Map<String, PasswordEncoder> encoders = new HashMap<>();
+//		encoders.put("pbkdf2", pbkdf2PasswordEncoder);
+//
+//		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+//		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder);
+//
+//		String result = passwordEncoder.encode("");
+//		System.out.printf(result);
+//	}
 }
