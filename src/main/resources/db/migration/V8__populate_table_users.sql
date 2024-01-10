1408084ebe6655d9e21ffb209d88f6ee647a4f4f
hiagobalbino <hiagobalbinoo@gmail.com>
2024-01-09 22:04:51 -0300

    fix: adjust user encrypted passcode

--- src/main/resources/db/migration/V8__populate_table_users.sql
+++ src/main/resources/db/migration/V8__populate_table_users.sql
@@ -1,3 +1,3 @@
 INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-    ('wes', 'Wes B.', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
-    ('tsu', 'Tsu R.', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
+    ('wes', 'Wes B.', '3111c7f785d6e97280028015f5a44f02ce9ddef9cb78f0bebb7354298cae714870af183c145f7a08', b'1', b'1', b'1', b'1'),
+    ('tsu', 'Tsu R.', '07f4d7480274aa2e11f21b0b8703c6b0449d1f267d55987764cc6d69cbafd8056938dbc7f03336ef', b'1', b'1', b'1', b'1');
