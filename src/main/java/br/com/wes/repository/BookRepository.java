19b79f5dc9b2e9f1ae1a06274fbb0cb0a9bd0ff6
hiagobalbino <hiagobalbinoo@gmail.com>
2024-01-21 16:30:09 -0300

    test: implement integration tests for bookRepository.findBooksByTitle

--- src/main/java/br/com/wes/repository/BookRepository.java
+++ src/main/java/br/com/wes/repository/BookRepository.java
@@ -1,8 +1,17 @@
 package br.com.wes.repository;
 
 import br.com.wes.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {}
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT b FROM Book b WHERE b.title LIKE LOWER(CONCAT('%',:title,'%'))")
+    Page<Book> findBooksByTitle(@Param("title") String title, Pageable pageable);
+
+}
---
+++ src/test/java/br/com/wes/integrationtests/repository/BookRepositoryIT.java
@@ -0,0 +1,47 @@
+package br.com.wes.integrationtests.repository;
+
+import br.com.wes.integrationtests.AbstractIT;
+import br.com.wes.model.Book;
+import br.com.wes.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookRepositoryIT extends AbstractIT {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void shouldReturnAllBooksWithSuccess() {
+        Pageable pageable = PageRequest.of(0, 6, Sort.by(Direction.ASC, "title"));
+        List<Book> books = bookRepository.findBooksByTitle("legacy code", pageable).getContent();
+        assertFalse(books.isEmpty());
+
+        Book book = books.getFirst();
+        assertEquals(1L, book.getId());
+        assertEquals("Working effectively with legacy code", book.getTitle());
+        assertEquals("Michael C. Feathers", book.getAuthor());
+        assertEquals(49.00, book.getPrice());
+        assertEquals(LocalDate.of(2017, 11, 29), book.getLaunchDate());
+    }
+
+    @Test
+    public void shouldReturnEmptyResultWhenNotFoundBooksByTitle() {
+        Pageable pageable = PageRequest.of(0, 6, Sort.by(Direction.ASC, "title"));
+        List<Book> books = bookRepository.findBooksByTitle("Unknown", pageable).getContent();
+        assertTrue(books.isEmpty());
+    }
+}
